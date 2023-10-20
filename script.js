de6f8aa1280141550a66d72f2e54f3dc7611900f
elene <gabidaidze1234@gmail.com>
2023-10-20 16:22:03 +0400

    make changes

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-// Get all year elements and year images
 const years = document.querySelectorAll('.year');
 const yearImages = document.querySelectorAll('.year-img');
 
-// Add click event listeners to each year element
 years.forEach((year, index) => {
     year.addEventListener('click', () => {
-        // Hide all year images
         yearImages.forEach(img => {
             img.style.display = 'none';
         });
-
-        // Remove the 'active' class from all years
         years.forEach(y => {
             y.classList.remove('active');
         });
-
-        // Show the corresponding year image
         yearImages[index].style.display = 'block';
-
-        // Add the 'active' class to the clicked year for animation
         year.classList.add('active');
     });
 });
@@ -28,19 +19,11 @@ const addresses = document.querySelectorAll(".address");
 
         addresses.forEach((address, index) => {
             address.addEventListener("click", () => {
-                // Hide all maps
                 maps.forEach(map => {
                     map.style.display = "none";
                 });
 
-                // Show the map corresponding to the clicked address
                 maps[index].style.display = "block";
             });
         });
-    
-
-
-
-
-
-
+  
